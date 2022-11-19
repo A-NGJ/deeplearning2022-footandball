dc469d421769b96f9b9f481413263d8c3565476b
Aleksander Nagaj <anagaj99@gmail.com>
2022-11-19 01:30:24 +0100

    soccer net loader class in progress

--- data/soccer_net.py
+++ data/soccer_net.py
@@ -1,49 +1,102 @@
 from collections import defaultdict
 import csv
 import os
+from pathlib import Path
 
-# pylint: disable=too-many-locals
-def collect(directory: str) -> defaultdict:
-    """
-    Collects data from soccer net data sets
+import numpy as np
+from PIL import Image
+import torch
 
-    Parameters
-    ----------
-    Data directory INSIDE soccer net directory. E.g. test, train
+from data.augmentation import (
+    PLAYER_LABEL,
+    BALL_LABEL,
+)
 
-    Requires
-    --------
-    ENV SOCCER_NET_PATH : absolute path to soccer net directory set as environment variable
 
-    Returns
-    -------
-    defaultdict:
-        Dictionary with image paths as keys and annotations of corner points as values (x1, y1, x2, y2)
-    """
+class SoccerNet(torch.utils.data.Dataset):
+    def __init__(self, data_path: str, transform=None):
+        self.image_list = []
+        self.gt = []
+        self.data_path = data_path
+        self.transform = transform
 
-    path = os.path
+    def __len__(self):
+        return len(self.image_list)
 
-    soccer_net_path = os.getenv("SOCCER_NET_PATH")
-    assert soccer_net_path, "missing env SOCCER_NET_PATH"
+    def __getitem__(self, idx: int):
+        img_path = self.image_list[idx]
+        image = Image.open(img_path)
+        boxes, labels = self.get_annotations(idx)
+        if self.transform is not None:
+            image, boxes, labels = self.transform((image, boxes, labels))
 
-    abs_path = path.join(soccer_net_path, directory)
+        boxes = torch.tensor(boxes, dtype=torch.float)
+        labels = torch.tensor(labels, dtype=torch.int64)
+        return image, boxes, labels
 
-    annotation_files = []
-    image_annotations = defaultdict(list)
+    # pylint: disable=too-many-locals
+    def collect(self) -> dict:
+        """
+        Collects data from soccer net data sets
 
-    for subdir in os.listdir(abs_path):
-        if "SNMOT" in subdir:
+        Requires
+        --------
+        ENV SOCCER_NET_PATH : absolute path to soccer net directory set as environment variable
+        """
+
+        path = os.path
+
+        soccer_net_path = os.getenv("SOCCER_NET_PATH")
+        assert soccer_net_path, "missing env SOCCER_NET_PATH"
+
+        abs_path = path.join(soccer_net_path, self.data_path)
+
+        annotation_files = []
+        image_annotations = defaultdict(list)
+
+        for subdir in Path(abs_path).glob("SNMOT*"):
             for file_ in os.listdir(path.join(abs_path, subdir, "det")):
                 annotation_files.append(path.join(abs_path, subdir, "det", file_))
 
-    for annotated_file in annotation_files:
-        st, sample = annotated_file.split("/")[-4:-2]
-        imgpath = "{0:s}/{1:s}/img1/{2:0>6d}.jpg"
-        with open(annotated_file, "r", encoding="utf-8") as rfile:
-            for row in csv.reader(rfile.readlines()):
-                frame, _, x, y, w, h = [int(x) for x in row[:6]]
-                image_annotations[imgpath.format(st, sample, frame)].append(
-                    (x, y, x + w, y + h)
-                )
+        for annotated_file in annotation_files:
+            st, sample = annotated_file.split("/")[-4:-2]
+            imgpath = "{0:s}/{1:s}/img1/{2:0>6d}.jpg"
 
-    return image_annotations
+            with open(annotated_file, "r", encoding="utf-8") as rfile:
+                for row in csv.reader(rfile.readlines()):
+                    frame, _, x, y, w, h = [int(x) for x in row[:6]]
+                    image_annotations[imgpath.format(st, sample, frame)].append(
+                        [x, y, x + w, y + h]
+                    )
+
+        for img, annot in image_annotations.items():
+            self.gt.append(np.array(annot))
+            self.image_list.append(img)
+
+    def get_annotations(self, idx: int):
+        """
+        Prepare annotations as list of boxes (xmin, ymin, xmax, ymax) in pixel coordinates
+        and torch int64 tensor of corresponding labels
+        """
+
+        bboxes = []
+        labels = []
+
+        min_ = np.inf
+        ball_idx = 0
+        # find the ball as a smallest bounding box
+        for (x1, y1, x2, y2) in self.gt[idx]:
+            size = x2 - x1 + y2 - y1
+            if size < min_:
+                min_ = size
+                ball_idx = idx
+
+        # Add annotations
+        for i, (x1, y1, x2, y2) in enumerate(self.gt[idx]):
+            bboxes.append((x1, y1, x2, y2))
+            if i == ball_idx:
+                labels.append(BALL_LABEL)
+            else:
+                labels.append(PLAYER_LABEL)
+
+        return np.array(bboxes, dtype=float), np.array(labels, dtype=np.int64)
--- misc/config.py
+++ misc/config.py
@@ -9,24 +9,30 @@ import time
 
 class Params:
     def __init__(self, path):
-        assert os.path.exists(path), "Cannot find configuration file: {}".format(path)
+        assert os.path.exists(path), f"Cannot find configuration file: {path}"
         self.path = path
 
         config = configparser.ConfigParser()
 
         config.read(self.path)
         params = config["DEFAULT"]
-        self.issia_path = params.get("issia_path", None)
+
+        # ISSIA
+        self.issia_path = os.path.expandvars(params.get("issia_path", None))
         if self.issia_path is not None:
             temp = params.get("issia_train_cameras", "1, 2, 3, 4")
             self.issia_train_cameras = [int(e) for e in temp.split(",")]
             temp = params.get("issia_val_cameras", "5, 6")
             self.issia_val_cameras = [int(e) for e in temp.split(",")]
 
-        self.spd_path = params.get("spd_path", None)
+        # SPD BMVC17
+        self.spd_path = os.path.expandvars(params.get("spd_path", None))
         if self.spd_path is not None:
             temp = params.get("spd_set", "1, 2")
             self.spd_set = [int(e) for e in temp.split(",")]
+
+        # SOCCER_NET
+        self.soccer_net_path = os.path.expandvars(params.get("soccer_net_path"))
 
         self.num_workers = params.getint("num_workers", 0)
         self.batch_size = params.getint("batch_size", 4)
@@ -34,37 +40,40 @@ class Params:
         self.lr = params.getfloat("lr", 1e-3)
 
         self.model = params.get("model", "fb1")
-        self.model_name = "model_{}_{}".format(self.model, get_datetime())
+        self.model_name = f"model_{self.model}_{get_datetime()}"
 
         self._check_params()
 
     def _check_params(self):
+        # ISSIA
         assert os.path.exists(
             self.issia_path
-        ), "Cannot access ISSIA CNR dataset: {}".format(self.issia_path)
+        ), f"Cannot access ISSIA CNR dataset: {self.issia_path}"
+        # SPD BMVC17
         assert os.path.exists(
             self.spd_path
-        ), "Cannot access SoccerPlayerDetection_bmvc17 dataset: {}".format(
-            self.spd_path
-        )
+        ), f"Cannot access SoccerPlayerDetection_bmvc17 dataset: {self.spd_path}"
+        # SOCCER_NET
+        assert os.path.exists(
+            self.soccer_net_path
+        ), f"Cannot access Soccer Net dataset: {self.soccer_net_path}"
+
         for c in self.issia_train_cameras:
             assert (
                 1 <= c <= 6
-            ), "ISSIA CNR camera number must be between 1 and 6. Is: {}".format(c)
+            ), f"ISSIA CNR camera number must be between 1 and 6. Is: {c}"
         for c in self.issia_val_cameras:
             assert (
                 1 <= c <= 6
-            ), "ISSIA CNR camera number must be between 1 and 6. Is: {}".format(c)
+            ), f"ISSIA CNR camera number must be between 1 and 6. Is: {c}"
         for c in self.spd_set:
-            assert c == 1 or c == 2, "SPD dataset number must be 1 or 2. Is: {}".format(
-                c
-            )
+            assert c == 1 or c == 2, f"SPD dataset number must be 1 or 2. Is: {c}"
 
     def print(self):
         print("Parameters:")
         param_dict = vars(self)
         for e in param_dict:
-            print("{}: {}".format(e, param_dict[e]))
+            print(f"{e}: {param_dict[e]}")
         print("")
 
 
