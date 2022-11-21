3463d64878769b4ae49934f23eb2dde78d293f59
Basia-P <78415069+Basia-P@users.noreply.github.com>
2022-11-21 16:24:43 +0100

    Hpc (#2)
    * branch for hpc
    
    * paths added

---
+++ venv_activate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+module load python3/3.8.11
+module load numpy/1.21.1-python-3.8.11-openblas-0.3.17
+module load scipy/1.6.3-python-3.8.11
+module load cuda/11.1
+module load matplotlib/3.4.2-numpy-1.21.1-python-3.8.11
+module load opencv/3.4.16-python-3.8.11-cuda-11.1
+
+source /work3/s210500/deeplearning2022-footandball/venv/bin/activate
