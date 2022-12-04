3f47be6615581bf590441f998020c0339b502594
Aleksander Nagaj <anagaj99@gmail.com>
2022-12-04 22:38:09 +0100

    add test results with detected ratio

--- run_detector_multiple.sh
+++ run_detector_multiple.sh
@@ -10,8 +10,8 @@ for j in {1..10}; do
     printf -v pad_j "%02d" $j
     for i in {116..125}; do
         python3 run_detector.py --path "${soccer_net_path}/SNMOT-${i}/img1/000001.jpg" \
-            --weights "${REPO}/models/train/20221201_2231_${pad_j}/model.pth" -o out.mp4 \
-            --device cpu --run-dir "${date}_${pad_j}_${i}" --player-threshold 0.9 \
+            --weights "${REPO}/models/model_20201019_1416_final.pth" -o out.mp4 \
+            --device cpu --run-dir "${date}_baseline_${i}" --player-threshold 0.9 \
             --metric-path "${soccer_net_path}/SNMOT-${i}"
     done
 done
